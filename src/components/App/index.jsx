3c2279f9937a1914c1ede0e1cab64e630a1a1267
V1codin <blindmonk46@gmail.com>
2020-10-05 13:40:11 +0300

    started developing form

---
+++ src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import style from "./styles.module.css";
+
+function App() {
+  return (
+    <div className={style.app}>
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
