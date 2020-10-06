b48badc471d02342671acbf10d6a03aed4cc2d32
V1codin <blindmonk46@gmail.com>
2020-10-06 21:13:40 +0300

    start

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,25 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: "./src/main.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
   watchOptions: {
     aggregateTimeout: 200,
     poll: 1000,
